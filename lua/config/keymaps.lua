bec6c0b5e949a4b69eb43fe492898bf573780641
yichun822 <yichun822@outlook.com>
2026-02-28 22:33:26 +0800

    快捷键修改

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--make快捷键
+require("which-key").add({
+  { "<leader>m", group = "Build" },
+  { "<Leader>mm", ":make<CR>", noremap = true, desc = "Build by Makefile" },
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- 改成四缩进
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
