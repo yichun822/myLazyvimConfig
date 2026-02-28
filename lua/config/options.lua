1d892ba72f630838faed9eba3c16a0fd98ff9246
yichun822 <yichun822@outlook.com>
2026-02-28 23:21:50 +0800

    缩进回调、README小改

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
--- 改成四缩进
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
