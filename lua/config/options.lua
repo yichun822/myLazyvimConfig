0111e4ec0be07def6daaa7ba28f3b3f77488431c
yichun822 <yichun822@outlook.com>
2026-03-06 21:00:42 +0800

    设置缩进

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 设置缩进
+vim.bo.tabstop = 4 -- Tab 显示的宽度
+vim.bo.shiftwidth = 4 -- 自动缩进的宽度
+vim.bo.softtabstop = 4 -- 编辑模式下按 Tab 占的宽度
+vim.bo.expandtab = true -- 将 Tab 转换为空格（推荐）
